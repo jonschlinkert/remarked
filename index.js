b5821d46b9c3ca39224da43519752e50b4b06e59
Kirill Efimov <kirill89@gmail.com>
2014-08-29 16:31:17 +0400

    API cleanup: removed async interface

--- index.js
+++ index.js
@@ -45,80 +45,16 @@ function Remarked(src, options, callback) {
  * parse
  */
 
-Remarked.prototype.parse = function(src, callback) {
-  if (!callback) {
-    try {
-      return this.parser.parse(this.lexer.lex(src));
-    } catch (e) {
-      e.message += '\n  [remarked]: please report this to https://github.com/jonschlinkert/remarked.';
-      if ((this.options || defaults).silent) {
-        return '<p>An error occured:</p><pre>' + utils._escape(e.message + '', true) + '</pre>';
-      }
-      throw e;
-    }
-  }
-
-  var self = this;
-  var highlight = this.options.highlight;
-  var tokens;
-  var pending;
-  var i = 0;
-
+Remarked.prototype.parse = function(src) {
   try {
-    tokens = this.lexer.lex(src);
+    return this.parser.parse(this.lexer.lex(src));
   } catch (e) {
-    return callback(e);
-  }
-
-  pending = tokens.length;
-
-  var cb = function (err) {
-    if (err) {
-      self.options.highlight = highlight;
-      return callback(err);
+    e.message += '\n  [remarked]: please report this to https://github.com/jonschlinkert/remarked.';
+    if ((this.options || defaults).silent) {
+      return '<p>An error occured:</p><pre>' + utils._escape(e.message + '', true) + '</pre>';
     }
-
-    var out;
-
-    try {
-      out = self.parser.parse(tokens);
-    } catch (e) {
-      err = e;
-    }
-
-    self.options.highlight = highlight;
-    return err ? callback(err) : callback(null, out);
-  };
-
-  if (!highlight || highlight.length < 3) {
-    return cb();
+    throw e;
   }
-
-  delete this.options.highlight;
-
-  if (!pending) {
-    return cb();
-  }
-
-  for (; i < pending; i += 1) {
-    var token = tokens[i];
-    if (token.type !== 'code') {
-      return (pending -= 1) || cb();
-    }
-    return highlight(token.text, token.lang, function (err, code) {
-      if (err) {
-        return cb(err);
-      }
-      if (code === null || code === token.text) {
-        return (pending -= 1) || cb();
-      }
-      token.text = code;
-      token.escaped = true;
-      (pending -= 1) || cb();
-    });
-  }
-
-  return;
 };
 
 
